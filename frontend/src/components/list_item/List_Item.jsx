6bd0d87ec62fc12ec6343f654caaaa64227f9528
Zeljko <00zeljkostojkovic@gmail.com>
2023-08-06 01:14:44 +0200

    Fixing object id

--- frontend/src/components/list_item/List_Item.jsx
+++ frontend/src/components/list_item/List_Item.jsx
@@ -1,11 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import "./List_Item.scss"
-import SpiderMan from "../../assets/images/marvel-spiderman.png"
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import AddIcon from '@mui/icons-material/Add';
 import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
 import ThumbDownAltOutlinedIcon from '@mui/icons-material/ThumbDownAltOutlined';
-import SpiderManVideo from "../../assets/videos/marvel-spiderman.mp4"
 
 const ListItem = ({ video }) => {
 
@@ -21,7 +19,7 @@ const ListItem = ({ video }) => {
             <img src={`${video.stream.gridFSFile.metadata?.photoUrl}`} alt="spiderman" />
             {isHovered && (
                 <>
-                    <video src={`http://localhost:8081/api/v1/video/stream/${video.stream.gridFSFile.metadata?.videoId}`} autoPlay={true} loop />
+                    <video src={`http://localhost:8081/api/v1/video/stream/${video.id}`} autoPlay={true} loop />
                     <div className="itemInfo">
                         <div className="icons">
                             <PlayArrowIcon className="icon" />
@@ -30,8 +28,8 @@ const ListItem = ({ video }) => {
                             <ThumbDownAltOutlinedIcon className="icon" />
                         </div>
                         <div className="itemInfoTop">
-                            <span>1 hour 14 mins</span>
-                            <span className="limit">+16</span>
+                            <span>{video.stream.gridFSFile.metadata?.duration}</span>
+                            <span className="limit">+{video.stream.gridFSFile.metadata?.ageRestriction}</span>
                             <span>1999</span>
                         </div>
                         <div className="desc">
--- video-service/src/main/java/com/zeljko/videoservice/service/VideoService.java
+++ video-service/src/main/java/com/zeljko/videoservice/service/VideoService.java
@@ -9,7 +9,6 @@ import com.zeljko.videoservice.model.Video;
 import com.zeljko.videoservice.repository.VideoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsOperations;
@@ -35,8 +34,6 @@ public class VideoService {
     public String addVideo(String title, String description, String photoUrl, String duration, Integer ageRestriction, String genre, MultipartFile videoFile) throws IOException {
 
         DBObject metaData = new BasicDBObject();
-        ObjectId id = new ObjectId();
-
         metaData.put("type", "video");
         metaData.put("title", title);
         metaData.put("description", description);
@@ -44,38 +41,21 @@ public class VideoService {
         metaData.put("duration", duration);
         metaData.put("ageRestriction", ageRestriction);
         metaData.put("genre", genre);
-        metaData.put("videoId", id.toString());
 
-        gridFsTemplate.store(videoFile.getInputStream(), videoFile.getName(), videoFile.getContentType(), metaData);
-
+        Object id = gridFsTemplate.store(
+                videoFile.getInputStream(), videoFile.getName(), videoFile.getContentType(), metaData);
 
         return id.toString();
-
     }
-
-//        Video video = Video.builder()
-//                .name(videoRequest.getName())
-//                .description(videoRequest.getDescription())
-//                .videoData(videoData)
-//                .category(videoRequest.getCategory())
-//                .duration(videoRequest.getDuration())
-//                .genre(videoRequest.getGenre())
-//                .ageRestriction(videoRequest.getAgeRestriction())
-//                .releasedYear(String.valueOf(videoRequest.getReleasedYear()))
-//                .creationDate(LocalDateTime.now())
-//                .build();
-//
-//        videoRepository.save(video);
-//        log.info("Video {} is saved", video.getId());
 
     public Video getVideo(String id) throws IllegalStateException, IOException {
         GridFSFile file = gridFsTemplate.findOne(new Query(Criteria.where("_id").is(id)));
         Video video = new Video();
+        video.setId(file.getObjectId().toString());
         video.setTitle(file.getMetadata().get("title").toString());
         video.setStream(gridFsOperations.getResource(file).getInputStream());
         return video;
     }
-
 
     public List<Video> getAllVideo() throws IOException {
         List<Video> videos = new ArrayList<>();
@@ -83,6 +63,7 @@ public class VideoService {
         GridFSFindIterable files = gridFsTemplate.find(new Query());
         files.forEach(file -> {
             Video video = new Video();
+            video.setId(file.getObjectId().toString());
             video.setTitle(file.getMetadata().get("title").toString());
             try {
                 video.setStream(gridFsTemplate.getResource(file).getInputStream());
@@ -96,23 +77,4 @@ public class VideoService {
     }
 }
 
-//    public List<VideoResponse> getAllVideos() {
-//       List<Video> videos = videoRepository.findAll();
-//
-//       return videos.stream().map(this::mapToVideoResponse).toList();
-//    }
-//
-//    private VideoResponse mapToVideoResponse(Video video) {
-//        return VideoResponse.builder()
-//                .id(video.getId())
-//                .name(video.getName())
-//                .videoData(video.getVideoData())
-//                .description(video.getDescription())
-//                .category(video.getCategory())
-//                .duration(video.getDuration())
-//                .genre(video.getGenre())
-//                .ageRestriction(video.getAgeRestriction())
-//                .creationDate(video.getCreationDate())
-//                .build();
-//    }
 
