5fadebedf1f1c63d561a8c76ede02d6995ca3dad
Zeljko <00zeljkostojkovic@gmail.com>
2023-08-27 13:22:57 +0200

    Initial commit for badge-service

---
+++ badge-service/src/test/java/com/zeljko/badgeservice/BadgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeljko.badgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
